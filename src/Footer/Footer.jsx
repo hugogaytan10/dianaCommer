241bf601876755c20a9fd7086d87ac441af83c64
[hugo gaytan] <[s19120145@alumnos.itsur.edu.mx]>
2024-03-26 18:16:19 -0600

    actualizacion

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,9 +4,7 @@ export const Footer = () => {
   return (
     <footer className='bg-primary h-52 w-full'>
       <h3 className="text-white text-center text-2xl font-bold uppercase mt-4">Diaz zapatos y accesorios</h3>
-      <h4 className="text-white w-3/4 text-center m-auto mt-2">
-        Adquiere los mejores zapatos y accesorios en nuestra tienda en línea
-      </h4>
+
       <div className="flex justify-center gap-2 mt-5">
         <a
           target="_blank"
