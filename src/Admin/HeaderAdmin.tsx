679dcfec5325a0aea3a1231175c398eda93fe272
[hugo gaytan] <[s19120145@alumnos.itsur.edu.mx]>
2024-08-02 17:44:03 -0600

    mejora en el panel de administracion

---
+++ src/Admin/HeaderAdmin.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import lupa from "./search.svg";
+import add from "./add.svg";
+import "./HeaderAdmin.css";
+import { set } from "@cloudinary/url-gen/actions/variable";
+
+type HeaderAdminProps = {
+  productosFiltrados: any[]; // Replace 'any' with the actual type of 'productosFiltrados'
+  productos: any[]; // Replace 'any' with the actual type of 'productos'
+  setProductosFiltrados: (productos: any[]) => void; // Replace 'any' with the actual type of 'productos'
+};
+
+export const HeaderAdmin = ({
+  productosFiltrados,
+  productos,
+  setProductosFiltrados
+}: HeaderAdminProps) => {
+  const [showInput, setShowInput] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+    //vamos a iterar sobre los productos y vamos a filtrar los que contengan el texto que estamos buscando
+    const filtro = productos.filter((producto) =>
+      producto.Titulo.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setProductosFiltrados(filtro);
+  };
+
+  return (
+    <div className="flex w-full justify-end gap-2 mt-4">
+      <button
+        className= "h-8 w-8 rounded-full bg-primary border-primary border-2 cursor-pointer flex items-center justify-center"
+        onClick={() =>
+          (
+            document.getElementById("modal_agregar") as HTMLDialogElement
+          )?.showModal?.()
+        }
+        tabIndex={0}
+      >
+        <p className="text-2xl text-gray-200">+</p>
+      </button>
+      <div className="relative flex items-center">
+        <img
+          src={lupa}
+          alt="Buscar"
+          className="z-10 h-8 w-8 rounded-full bg-white border-primary border-2 cursor-pointer flex items-center justify-center"
+          onClick={(e)=>{setShowInput(!showInput)}}
+        />
+        <input
+          type="text"
+          className={`search-input ${showInput ? "expanded" : "collapsed"}`}
+          placeholder="Buscar..."
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+      </div>
+    </div>
+  );
+};
