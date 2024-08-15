6898360bc18c669807fb47d82065a096bd8f6a0d
LuisJLG <Ls19120191@surguanajuato.tecnm.mx>
2024-08-14 18:31:05 -0600

    modal agregar categoria en proceso de adicion de multiples subcategorias

--- src/Admin/Categorias/ModalAgregarCategoria.jsx
+++ src/Admin/Categorias/ModalAgregarCategoria.jsx
@@ -7,6 +7,7 @@ export const ModalAgregarCategoria = ({ actualizar, setActualizar }) => {
   const [nombre, setNombre] = useState("");
   const [nombreSubcategoria, setNombreSubcategoria] = useState("");
   const [subcategorias, setSubcategorias] = useState([]);
+  const [errorSubcategoria, setErrorSubcategoria] = useState(false);
   const context = useContext(AppContext);
 
   const InsertarCategoria = async (e) => {
@@ -16,7 +17,7 @@ export const ModalAgregarCategoria = ({ actualizar, setActualizar }) => {
         Nombre: nombre
       },
       Subcategories: [{
-        Nombre:nombreSubcategoria,
+        Nombre: nombreSubcategoria,
       }]
     };
     if (
@@ -98,6 +99,92 @@ export const ModalAgregarCategoria = ({ actualizar, setActualizar }) => {
                   </option>
                 ))}
               </select>
+              <button
+                type="button"
+                onClick={() => {
+                  if (document.getElementById("subcategoria").value === "" || document.getElementById("stock").value === "") {
+                    setErrorSubcategoria(true);
+                    return;
+                  }
+                  const nuevaSubcategoria = {
+                    Subcategoria: document.getElementById("subcategoria").value,
+                    Stock: document.getElementById("stock").value,
+                  };
+                  setSubcategorias([...subcategorias, nuevaSubcategoria]);
+                  document.getElementById("subcategoria").value = "";
+                  document.getElementById("stock").value = "";
+                  setErrorSubcategoria(false);
+                }}
+                className="btn-siguiente h-12 text-primary border-none w-1/4"
+              ></button>
+            </div>
+
+            <div className="form-group w-full flex flex-wrap items-end gap-2 mb-0">
+              <div className="form-group w-1/4 mb-0">
+                <input
+                  type="number"
+                  placeholder=" "
+                  id="subcategoria"
+                  name="subcategoria"
+                  className="bg-white "
+                />
+                <label>Subcategoria</label>
+              </div>
+              <div className="form-group w-1/4 mb-0">
+                <input
+                  type="number"
+                  placeholder=" "
+                  id="stock"
+                  name="stock"
+                  className="bg-white"
+                  onChange={(e) => {
+                    setStock(e.target.value);
+                  }}
+                />
+                <label>Stock</label>
+              </div>
+              <button
+                type="button"
+                onClick={() => {
+                  if (document.getElementById("subcategoria").value === "" || document.getElementById("stock").value === "") {
+                    setErrorSubcategoria(true);
+                    return;
+                  }
+                  const nuevaSubcategoria = {
+                    Subcategoria: document.getElementById("subcategoria").value,
+                    Stock: document.getElementById("stock").value,
+                  };
+                  setSubcategorias([...subcategorias, nuevaSubcategoria]);
+                  document.getElementById("subcategoria").value = "";
+                  document.getElementById("stock").value = "";
+                  setErrorSubcategoria(false);
+                }}
+                className="btn-siguiente h-12 text-primary border-none w-1/4"
+              >
+                Agregar
+              </button>
+              {
+                <p className="text-red-500  text-xs block w-full">
+                  {errorSubcategoria ? "Debe agregar al menos una subcategoria" : ""}
+                </p>
+              }
+            </div>
+            <div className="flex flex-wrap">
+              {subcategorias &&
+                subcategorias.length > 0 &&
+                subcategorias.map((subcategoria, index) => (
+                  <div className="btn-subcategoria" key={`subcategoria-${subcategoria.Subcategoria}`}>
+                    <p id={`subcategoria-${subcategoria}`}>{subcategoria.Subcategoria}</p>
+                    <button
+                      className="btn-eliminar-subcategoria"
+                      onClick={() => {
+                        setSubcategorias(subcategorias.filter((t) => t !== subcategoria));
+                      }}
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
             </div>
 
             <div className="flex w-full justify-around">
